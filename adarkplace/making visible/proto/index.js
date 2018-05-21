874aed57beea1aeb4696788bc488bbe2c30dcdef
Federico Cortesi <31067595+adarkplace@users.noreply.github.com>
2018-05-21 20:09:53 +0200

    Update index.js

--- adarkplace/making visible/proto/index.js
+++ adarkplace/making visible/proto/index.js
@@ -1,11 +1,13 @@
 // var map = L.map('mapid').setView([51.505, -0.09], 13);
 var bounds = [[-1200,-1200], [1200,1200]];
-var imageURL = 'https://eoimages.gsfc.nasa.gov/images/imagerecords/78000/78592/antarctica_etm_2000001_lrg.jpg';
+// var imageURL = 'https://eoimages.gsfc.nasa.gov/images/imagerecords/78000/78592/antarctica_etm_2000001_lrg.jpg';
+var imageURL = 'img/antarctica_3.jpg';
+
 
 
 var map = L.map('mapid', {
   crs: L.CRS.Simple,
-  center: [650, -350],
+  center: [-538, -188],
   zoom:2,
   maxBounds: bounds,
   maxBoundsViscosity: 1,
@@ -18,18 +20,27 @@ var image = L.imageOverlay(imageURL, bounds).addTo(map);
 // map.fitBounds(bounds);
 
 
-var sol = L.latLng([ 0, 0 ]);
+var sol = L.latLng([ 2, -20 ]);
 L.marker(sol).addTo(map);
 
-var sol = L.latLng([650, -350]);
+var sol = L.latLng([-163, -58]);
+L.marker(sol).addTo(map);
+
+var sol = L.latLng([-206, -71]);
+L.marker(sol).addTo(map);
+
+var sol = L.latLng([-263, -89]);
+L.marker(sol).addTo(map);
+
+var sol = L.latLng([-333, -109]);
+L.marker(sol).addTo(map);
+
+var sol = L.latLng([-538, -188]);
 L.marker(sol).addTo(map);
 
 // var sol = L.latLng([ 200, 200 ]);
 // L.marker(sol).addTo(map);
 // map.setView( [70, 120], 1);
-
-
-
 
 var popup = L.popup();
 function onMapClick(e) {
@@ -39,3 +50,14 @@ function onMapClick(e) {
         .openOn(map);
 }
 map.on('click', onMapClick);
+
+var locationI = L.icon({
+    iconUrl: 'img/pin.png',
+    iconSize: [42, 78],
+    iconAnchor: [21, 0],
+    popupAnchor: [-3, -76],
+    shadowUrl: 'img/pin.png',
+    shadowSize: [42, 78],
+    shadowAnchor: [21, 0]
+});
+L.marker([0,50], {icon: locationI}).addTo(map);
